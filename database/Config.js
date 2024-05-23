bce1c638e9130ef08c5670e79d87461d64b5fb5e
ccmacas <celiamacas24@gmail.com>
2024-05-22 19:48:04 -0500

    Pruebas unitarias

--- database/Config.js
+++ database/Config.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//const URI= 'mongodb+srv://celiamacas24:4bjUWRLzea9GibaJ@minig.soqbw3p.mongodb.net/MinigCompany?retryWrites=true&w=majority&appName=Minig ';
-const URI = 'mongodb://localhost:27017/MinigCompany';
+const URI= 'mongodb+srv://celiamacas24:4bjUWRLzea9GibaJ@minig.soqbw3p.mongodb.net/MinigCompany?retryWrites=true&w=majority&appName=Minig ';
+//const URI = 'mongodb://localhost:27017/MinigCompany';
 const connection = mongoose.connect(
     URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Conectado a MongoDB'))
